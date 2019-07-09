b6372691c08ff8931676cda610a9bf5ad50bcd9a
Alexander Bazo <alexanderbazo@googlemail.com>
2019-07-09 15:08:18 +0200

    Add simple solution

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,38 @@
 /*eslint-env browser */
 
+import GameManger from "./game/GameManger.js";
+import GameView from "./ui/GameView.js";
+import ScoreView from "./ui/ScoreView.js";
+
 function init() {
-    console.log("### Starting Color Clicker game");
+  ScoreView.setElement(document.querySelector("#score"));
+  GameView.setElement(document.querySelector("#squares"));
+  GameView.addEventListener("squareclicked", onSquareClicked);
+  GameManger.addEventListener("gamestarted", onGameStarted);
+  GameManger.addEventListener("levelcleared", onLevelCleared);
+  GameManger.addEventListener("gamelost", onGameLost);
+  GameManger.reset();
+}
+
+function renderState(level, score, squares) {
+  ScoreView.renderScore(score);
+  GameView.renderSquares(squares);
+}
+
+function onSquareClicked(event) {
+  GameManger.checkClickedID(event.data.id);
+}
+
+function onGameStarted(event) {
+  renderState(event.data.currentLevel, event.data.currentScore, event.data.squares);
+}
+
+function onLevelCleared(event) {
+  renderState(event.data.currentLevel, event.data.currentScore, event.data.squares);
+}
+
+function onGameLost(event) {
+  GameManger.reset();
 }
 
 init();
