a1977e778f4b83c0fd9b37dbb19e7c318342b14f
Alexander Bazo <alexanderbazo@googlemail.com>
2020-11-11 10:46:45 +0100

    Update code for new class

--- resources/js/game/Gamestate.js
+++ resources/js/game/Gamestate.js
@@ -3,13 +3,13 @@
 import DEFAULTS from "./Defaults.js";
 
 class Gamestate {
-  constructor() {
-    this.level = DEFAULTS.startLevel;
-    this.deviation = DEFAULTS.startDeviation;
-    this.numberOfSquares = DEFAULTS.squaresPerLevel[0];
-    this.squares = undefined;
-    this.targetID = undefined;
-  }
+    constructor() {
+        this.level = DEFAULTS.startLevel;
+        this.deviation = DEFAULTS.startDeviation;
+        this.numberOfSquares = DEFAULTS.squaresPerLevel[0];
+        this.squares = undefined;
+        this.targetID = undefined;
+    }
 }
 
 export default Gamestate;
--- resources/js/ui/ScoreView.js
+++ resources/js/ui/ScoreView.js
@@ -2,21 +2,21 @@
 
 class ScoreView {
 
-  setElement(el) {
-    this.el = el;
-    this.currentScore = this.el.querySelector("#current-score");
-    this.highscore = this.el.querySelector("#highscore");
-  }
+    setElement(el) {
+        this.el = el;
+        this.currentScore = this.el.querySelector("#current-score");
+        this.highscore = this.el.querySelector("#highscore");
+    }
 
-  renderScore(score) {
-    this.currentScore.innerHTML = score;
-    this.highscore.innerHTML = score;
-  }
+    renderScore(score) {
+        this.currentScore.innerHTML = score;
+        this.highscore.innerHTML = score;
+    }
 
-  reset() {
-    this.currentScore.innerHTML = "0";
-    this.highscore.innerHTML = "-";
-  }
+    reset() {
+        this.currentScore.innerHTML = "0";
+        this.highscore.innerHTML = "-";
+    }
 
 }
 
